35fd869f486e59d98b12254286afb74f39cf7c75
bharath <naveenbharath93@gmail.com>
2019-04-23 22:38:22 +0530

    service file updated

--- services/fileupload-service.js
+++ services/fileupload-service.js
@@ -1,54 +1,26 @@
 const convert = require("xml-js");
 /*
 - File Upload method which validate both XML and CSV format,
-- It will validate all transaction reference no should be unique,
-- and end balance should be validated based on start balance & mutation values.
+- and transforms key attributes based on uploaded file.
+- @params file
 */
 const FileUpload = file => {
   return new Promise(resolve => {
     let records = [];
-    const errorRecords = [];
     if (file.name.indexOf(".xml") !== -1) {
       // XML File Validations
       const data = file.data.toString();
       records = JSON.parse(convert.xml2json(data, { compact: true, spaces: 4 }))
         .records.record;
-      for (const record of records) {
-        let isUnique = true;
-        let isEndBalanceCorrect = true;
-        // Transaction Reference Number Unique Validation
-        if (
-          record._attributes.reference &&
-          records.filter(
-            value =>
-              value._attributes.reference === record._attributes.reference
-          ).length > 1
-        ) {
-          isUnique = false;
-        }
-        // End Balance Validation
-        if (
-          (
-            parseFloat(record.startBalance._text) +
-            parseFloat(record.mutation._text)
-          ).toFixed(2) !== parseFloat(record.endBalance._text).toFixed(2)
-        ) {
-          isEndBalanceCorrect = false;
-        }
-        if (!isUnique || !isEndBalanceCorrect) {
-          const error =
-            !isUnique && !isEndBalanceCorrect
-              ? "Duplicate Reference No, Incorrect End Balance"
-              : !isUnique && isEndBalanceCorrect
-              ? "Duplicate Reference No"
-              : "Incorrect End Balance";
-          errorRecords.push({
-            Reference: record._attributes.reference,
-            Description: record.description._text,
-            error
-          });
-        }
-      }
+      const transformed = records.map(key => ({
+        Reference: key._attributes.reference,
+        Start_Balance: key.startBalance._text,
+        Mutation: key.mutation._text,
+        End_Balance: key.endBalance._text,
+        Description: key.description._text
+      }));
+      const finalValue = uniqueValidation(transformed);
+      return resolve(finalValue);
     } else if (file.name.indexOf(".csv")) {
       // CSV File Validations
       const rows = file.data
@@ -78,43 +50,52 @@ const FileUpload = file => {
           records.push(record);
         }
       }
-      for (const record of records) {
-        let isUnique = true;
-        let isEndBalanceCorrect = true;
-        // Transaction Reference Number Unique Validation
-        if (
-          record.Reference &&
-          records.filter(value => value.Reference === record.Reference).length >
-            1
-        ) {
-          isUnique = false;
-        }
-        // End Balance Validation
-        if (
-          (
-            parseFloat(record.Start_Balance) + parseFloat(record.Mutation)
-          ).toFixed(2) !== parseFloat(record.End_Balance).toFixed(2)
-        ) {
-          isEndBalanceCorrect = false;
-        }
-        if (!isUnique || !isEndBalanceCorrect) {
-          const error =
-            !isUnique && !isEndBalanceCorrect
-              ? "Duplicate Reference No, Incorrect End Balance"
-              : !isUnique && isEndBalanceCorrect
-              ? "Duplicate Reference No"
-              : "Incorrect End Balance";
-          errorRecords.push({
-            Reference: record.Reference,
-            Description: record.Description,
-            error
-          });
-        }
-      }
+      const finalValue = uniqueValidation(records);
+      return resolve(finalValue);
     }
-    return resolve(errorRecords);
   });
 };
+/*
+- uniqueValudaton method will validate all transaction reference no should be unique,
+- and end balance should be validated based on start balance & mutation values.
+- @params records
+*/
+const uniqueValidation = records => {
+  const errorRecords = [];
+  for (const record of records) {
+    let isUnique = true;
+    let isEndBalanceCorrect = true;
+    // Transaction Reference Number Unique Validation
+    if (
+      record.Reference &&
+      records.filter(value => value.Reference === record.Reference).length > 1
+    ) {
+      isUnique = false;
+    }
+    // End Balance Validation
+    if (
+      (
+        parseFloat(record.Start_Balance) + parseFloat(record.Mutation)
+      ).toFixed(2) !== parseFloat(record.End_Balance).toFixed(2)
+    ) {
+      isEndBalanceCorrect = false;
+    }
+    if (!isUnique || !isEndBalanceCorrect) {
+      const error =
+        !isUnique && !isEndBalanceCorrect
+          ? "Duplicate Reference No, Incorrect End Balance"
+          : !isUnique && isEndBalanceCorrect
+            ? "Duplicate Reference No"
+            : "Incorrect End Balance";
+      errorRecords.push({
+        Reference: record.Reference,
+        Description: record.Description,
+        error
+      });
+    }
+  }
+  return errorRecords;
+}
 
 module.exports = {
   FileUpload
